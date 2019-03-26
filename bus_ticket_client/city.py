3da94aea3f5925b2c6fe352c2ad75b04a99dfa7b
jmcalves <jmcalves275@gmail.com>
2019-03-26 22:19:31 +0000

    Revert "test"
    This reverts commit 27f10d198857893594746a4b415793d16185a0e4.

---
+++ bus_ticket_client/city.py
@@ -0,0 +1,10 @@
+class City(object):
+  def __init__(self, id, name):
+    self.id = id
+    self.name = name
+  
+  def getID(self):
+    return self.id
+    
+  def getName(self):
+    return self.name
