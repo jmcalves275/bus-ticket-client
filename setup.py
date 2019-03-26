88f3a6a3139d0249716adf6fcf83c73be3fd67fd
jmcalves <jmcalves275@gmail.com>
2019-03-26 19:45:25 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='bus_ticket_client',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/jmcalves275/bus_ticket_client',
+      author='jmcralves',
+      author_email='jmcralves@gmail.com',
+      packages=['bus_ticket_client'],
+      zip_safe=False)
