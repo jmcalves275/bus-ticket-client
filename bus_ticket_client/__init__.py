55215371682adc83b0bcb09f22034c574b6e8ac0
jmcalves <jmcalves275@gmail.com>
2019-03-26 22:12:41 +0000

    test commit

--- bus_ticket_client/__init__.py
+++ bus_ticket_client/__init__.py
@@ -1,7 +1,11 @@
 class Client(object):
 
-  def __init__(self, name):
+  def __init__(self, name, city):
     self.name = name
+    self.city = city
   
   def getName(self):
     return self.name
+
+  def printCityName(self):
+    print (self.city)
